8c1116eb558cfac544b6191defb6dceada3a1667
Iris <marlx42@protonmail.com>
2022-05-30 10:59:01 +0200

    First

---
+++ script.js
@@ -0,0 +1,20 @@
+// let footer = document.querySelector('footer');
+// footer.addEventListener('click',wordAppear);
+
+// function wordAppear() {
+//  console.log(document.innerHTML = 'Clique');
+// }
+
+// footer.onclick = wordAppear;
+
+let footer = document.querySelector('footer');
+footer.addEventListener('click',wordAppear);
+
+function wordAppear() {
+  for(i > 0; ; i++);{
+    console.log(document.innerHTML = 'Clique');
+
+  }
+}
+
+footer.onclick = wordAppear;
